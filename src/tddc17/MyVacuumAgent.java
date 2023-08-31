b3edb9205959ed6c6e6c43e1bd42f88c03a2d973
Mohamed Elmdimegh <mohel393@student.liu.se>
2023-08-31 11:13:36 +0200

    first commit

---
+++ src/tddc17/MyVacuumAgent.java
@@ -0,0 +1,10 @@
+package tddc17;
+
+import aima.core.agent.impl.AbstractAgent;
+import tddc17.MyAgentProgram;
+
+public class MyVacuumAgent extends AbstractAgent {
+    public MyVacuumAgent() {
+        super(new MyAgentProgram());
+    }
+}
